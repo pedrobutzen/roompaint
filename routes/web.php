58162aa0946edf484ba10582263be2e81af83fc4
Pedro Butzen <pedrobutzen@outlook.com>
2021-09-04 23:15:49 -0300

    Overwriting rota login pra rota root

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Auth::routes([
     'verify' => false
 ]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\Auth\LoginController::class, 'showLoginForm'])->name('login');
 
 Route::group(['middleware' => 'auth'], function () {
     Route::get('/cores', [App\Http\Controllers\HomeController::class, 'index'])->name('cores');
